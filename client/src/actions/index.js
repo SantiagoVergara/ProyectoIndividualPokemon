9bdbdcde27b1b27251c1aead98160868690bd6fa
Santiago Vergara <santivergara.27@gmail.com>
2022-09-28 18:26:11 -0300

    nuevas rutas back y front

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export function getPokemons(){
     return async function(dispatch){
-        var json = await axios.get("http://localhost:3001/pokemons",{});
+        var json = await axios.get("https://pi-pokemon-back.onrender.com/pokemons",{});
         return dispatch({
             type: 'GET_POKEMONS',
             payload: json.data
@@ -12,7 +12,7 @@ export function getPokemons(){
 
 export function getPokemonsTypes(){
     return async function(dispatch){
-        var json = await axios.get("http://localhost:3001/tipos",{});
+        var json = await axios.get("https://pi-pokemon-back.onrender.com/tipos",{});
         return dispatch({
             type: 'GET_TYPES',
             payload: json.data
@@ -51,7 +51,7 @@ export function orderByAttack(payload){
 export function getNamePokemon(name){
     return async function (dispatch){
         try {
-            var json = await axios.get("http://localhost:3001/pokemons?name="+name);
+            var json = await axios.get("https://pi-pokemon-back.onrender.com/pokemons?name="+name);
             return dispatch({
                 type: 'GET_NAME_POKEMON',
                 payload: json.data
@@ -65,7 +65,7 @@ export function getNamePokemon(name){
 export function getDetail(id){
     return async function (dispatch){
         try{
-            var json = await axios.get("http://localhost:3001/pokemons/"+id);
+            var json = await axios.get("https://pi-pokemon-back.onrender.com/pokemons/"+id);
             return dispatch ({
                 type: 'GET_DETAILS',
                 payload: json.data
@@ -78,7 +78,7 @@ export function getDetail(id){
 
 export function postPokemon(payload){
     return async function (dispatch){
-        const json = await axios.post("http://localhost:3001/pokemons/",payload);
+        const json = await axios.post("https://pi-pokemon-back.onrender.com/pokemons/",payload);
         console.log(json);
         return json;
     }
